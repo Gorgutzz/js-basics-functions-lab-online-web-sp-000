c67f9d75605a8e269e9ac6f4e04cb8ed712ff405
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-08 01:17:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ function distanceFromHqInBlocks(distance) {
   return Math.abs(distance - 42);
 }
 
-function distanceFromHqInFeet (someValue) {
-  distanceFromHqInBlocks(someValue);
-  // calls distanceFromHqInBlocks from inside the distanceFromHqInFeet function, passing the argument from distanceFromHqInFeet into distanceFromHqInBlocks
-  //the return value of distanceFromHqInBlocks can then be used to calculate feet
+function distanceFromHqInFeet(distance) {
+  return distanceFromHqInBlocks(distance)*264;
 }
